1c861c8b9e21d8fef5f1844c9b50ef4a0e0094c5
Vinaysingh09 <vinaybrothervns@gmail.com>
2025-08-06 00:06:35 +0530

    some changes

--- ServicePayment/src/main/java/com/controller/Controller.java
+++ ServicePayment/src/main/java/com/controller/Controller.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class Controller {
 	}
 	
 	@PostMapping(value = "/process")
-	public PaymentRequest ProcesPaymentService(PaymentRequest req) {
+	public PaymentRequest ProcesPaymentService(@RequestBody PaymentRequest req) {
 		return paymentService.processPayment(req);
 	}
 }
--- ServiceUser/src/main/java/com/services/IUserPayment.java
+++ ServiceUser/src/main/java/com/services/IUserPayment.java
@@ -1,11 +1,13 @@
 package com.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.common.model.OrderReceivedRequest;
 
 @FeignClient(name = "payment-service",url = "http://payment-service:8080")
 public interface IUserPayment {
+    @PostMapping("/pay/process")
 	OrderReceivedRequest processPayment(@RequestBody OrderReceivedRequest req);
 }
